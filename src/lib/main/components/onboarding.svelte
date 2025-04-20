7990346dd04bbb2df401c6cd589e5ed3e6e32e8a
calebeallen <calebeallen318@gmail.com>
2025-04-20 17:53:38 -0400

    small changes

--- src/lib/main/components/onboarding.svelte
+++ src/lib/main/components/onboarding.svelte
@@ -32,9 +32,9 @@
 <svelte:document on:click={enter} on:keydown={enter}/>
 
 <div out:fade class="fixed flex items-center justify-center w-screen h-screen bg-black">
-    <div class="flex flex-col items-center gap-4 font-bold text-center texl-lg sm:text-2xl">
+    <div class="flex flex-col items-center gap-4 font-bold text-center texl-xl sm:text-2xl">
         <div class="{show > 0 ? "opacity-100" : "opacity-0"} transition-opacity">How it works:</div>
-        <div class="{show > 1 ? "opacity-100" : "opacity-0"}  transition-opacity">1. Claim any plot</div>
+        <div class="{show > 1 ? "opacity-100" : "opacity-0"}  transition-opacity">1. Claim any open plot</div>
         <div class="{show > 2 ? "opacity-100" : "opacity-0"}  transition-opacity">2. Build anything on it</div>
         <button class="{show > 3 ? "opacity-100 animate-pulse" : "opacity-0"} transition-opacity flex items-center gap-1 hover:opacity-50 mt-4 ">
             <div>Got it!</div>

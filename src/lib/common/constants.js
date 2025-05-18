2bb112ce7d9c54296a09d30afb0e37b9d30e7ea4
calebeallen <calebeallen318@gmail.com>
2025-05-18 13:34:53 -0400

    updated build size limit

--- src/lib/common/constants.js
+++ src/lib/common/constants.js
@@ -16,8 +16,8 @@ const MAX_DEPTH = 2
 const D0_PLOT_COUNT = 34998
 const PLOT_COUNT = 24
 
-const BUILD_SIZE_STD = 32
-const BUILD_SIZE_LARGE = 64
+const BUILD_SIZE_STD = 48
+const BUILD_SIZE_LARGE = 72
 const MIN_BUILD_SIZE = 6
 const LOW_RES = 2
 const CHUNK_SIZE = 6
